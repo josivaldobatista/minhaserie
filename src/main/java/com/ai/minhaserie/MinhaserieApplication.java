4b5139d89e241eb7fc877011d28377afd1976ccf
Josivaldo Batista <josivaldo-fb@outlook.com>
2024-06-20 12:58:11 -0300

    first commit

---
+++ src/main/java/com/ai/minhaserie/MinhaserieApplication.java
@@ -0,0 +1,13 @@
+package com.ai.minhaserie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhaserieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhaserieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ai/minhaserie/MinhaserieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.minhaserie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhaserieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
